a1a8f3b64cd879ad83b826438e261d7c2328d77c
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 08:40:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import streamlit as st
 import requests
 
-def get_price(coin):
-    url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin}&vs_currencies=usd"
+def get_price(coin, currency):
+    url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin}&vs_currencies={currency}"
     response = requests.get(url)
     data = response.json()
-    price = data[coin]['usd']
+    price = data[coin][currency]
     return price
 
 def main():
     st.title("Harga BCD dan BAT")
-    bcd_price = get_price('bitcoin-diamond')
-    bat_price = get_price('basic-attention-token')
     
-    st.write("Harga Bitcoin Diamond (BCD): $", bcd_price)
-    st.write("Harga Basic Attention Token (BAT): $", bat_price)
+    currency = "idr"  # Ubah sesuai dengan kode mata uang yang kamu inginkan
+    
+    bcd_price = get_price('bitcoin-diamond', currency)
+    bat_price = get_price('basic-attention-token', currency)
+    
+    st.write("Harga Bitcoin Diamond (BCD):", currency.upper(), bcd_price)
+    st.write("Harga Basic Attention Token (BAT):", currency.upper(), bat_price)
 
 if __name__ == '__main__':
     main()
