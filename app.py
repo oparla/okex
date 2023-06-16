df868026c76dcefaf4c6bfca4cf73c846e1e5c2b
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 09:41:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import time
 
 # Menampilkan animasi balon
-balloon = st.balloons()
+st.balloons()
 
 # Memberikan jeda selama 2 detik sebelum menampilkan balon meledak
 time.sleep(2)
 
-# Menampilkan balon meledak
-st.balloons(balloon)
+# Menghapus animasi balon
+st.balloons(0)
