be048a7b4cd8d238f767b9e51e6b2c165406fa17
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 08:44:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,40 @@
 import streamlit as st
-import requests
-import locale
 
-def get_price(coin, currency):
-    url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin}&vs_currencies={currency}"
-    response = requests.get(url)
-    data = response.json()
-    price = data[coin][currency]
-    return price
+def add(a, b):
+    return a + b
 
-def format_currency(price):
-    locale.setlocale(locale.LC_ALL, 'id_ID')  # Setel lokalisasi ke bahasa Indonesia
-    formatted_price = locale.currency(price, grouping=True)
-    return formatted_price
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
 
 def main():
-    st.title("Harga BCD dan BAT")
+    st.title("Kalkulator Sederhana")
     
-    currency = "idr"  # Ubah sesuai dengan kode mata uang yang kamu inginkan
+    num1 = st.number_input("Masukkan angka pertama:")
+    num2 = st.number_input("Masukkan angka kedua:")
     
-    try:
-        bcd_price = get_price('bitcoin-diamond', currency)
-        bat_price = get_price('basic-attention-token', currency)
-        
-        formatted_bcd_price = format_currency(bcd_price)
-        formatted_bat_price = format_currency(bat_price)
-        
-        st.write("Harga Bitcoin Diamond (BCD):", formatted_bcd_price)
-        st.write("Harga Basic Attention Token (BAT):", formatted_bat_price)
-    except KeyError:
-        st.write("Tidak dapat memperoleh data harga saat ini.")
+    operation = st.selectbox("Pilih operasi:", ("Penjumlahan", "Pengurangan", "Perkalian", "Pembagian"))
+    
+    result = None
+    
+    if operation == "Penjumlahan":
+        result = add(num1, num2)
+    elif operation == "Pengurangan":
+        result = subtract(num1, num2)
+    elif operation == "Perkalian":
+        result = multiply(num1, num2)
+    elif operation == "Pembagian":
+        result = divide(num1, num2)
+    
+    st.write("Hasil:", result)
 
 if __name__ == '__main__':
     main()
