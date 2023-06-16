24b200a6ae0f28944bd33def8107f76b405846a5
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 08:42:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import locale
 
 def get_price(coin, currency):
     url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin}&vs_currencies={currency}"
@@ -7,6 +8,11 @@ def get_price(coin, currency):
     data = response.json()
     price = data[coin][currency]
     return price
+
+def format_currency(price):
+    locale.setlocale(locale.LC_ALL, 'id_ID')  # Setel lokalisasi ke bahasa Indonesia
+    formatted_price = locale.currency(price, grouping=True)
+    return formatted_price
 
 def main():
     st.title("Harga BCD dan BAT")
@@ -17,8 +23,11 @@ def main():
         bcd_price = get_price('bitcoin-diamond', currency)
         bat_price = get_price('basic-attention-token', currency)
         
-        st.write("Harga Bitcoin Diamond (BCD):", currency.upper(), bcd_price)
-        st.write("Harga Basic Attention Token (BAT):", currency.upper(), bat_price)
+        formatted_bcd_price = format_currency(bcd_price)
+        formatted_bat_price = format_currency(bat_price)
+        
+        st.write("Harga Bitcoin Diamond (BCD):", formatted_bcd_price)
+        st.write("Harga Basic Attention Token (BAT):", formatted_bat_price)
     except KeyError:
         st.write("Tidak dapat memperoleh data harga saat ini.")
 
