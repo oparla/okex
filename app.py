d4551d1c0bea327622944ab403b2c7b4f96eea66
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 09:16:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,35 @@
 import streamlit as st
 import pandas as pd
 
-def get_transaction_history(url):
-    try:
-        df = pd.read_html(url)[0]  # Membaca tabel dari URL
+# Import library untuk melakukan HTTP request
+import requests
+from bs4 import BeautifulSoup
+
+# Fungsi untuk melakukan scraping dan mengambil data tabel
+def scrape_table(url):
+    # Mengirim HTTP GET request ke URL
+    response = requests.get(url)
+    
+    # Mengecek status code response
+    if response.status_code == 200:
+        # Membuat objek BeautifulSoup untuk melakukan parsing HTML
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # Mencari tabel berdasarkan class
+        table = soup.find('table', class_='table table-striped')
+        
+        # Membaca tabel dan mengkonversikannya menjadi DataFrame
+        df = pd.read_html(str(table))[0]
+        
         return df
-    except Exception as e:
-        st.error(f"Terjadi kesalahan dalam memuat tabel: {e}")
-        return None
-
-def main():
-    st.title("Data Transaksi")
-
-    url = "https://pgkreload.com/history"  # Ganti dengan URL yang sesuai
-    df = get_transaction_history(url)
-
-    if df is not None:
-        st.write("Data Transaksi:")
-        st.dataframe(df)
     else:
-        st.warning("Tidak ada data transaksi yang tersedia.")
+        st.error(f'Error: {response.status_code}')
 
-if __name__ == "__main__":
-    main()
+# URL yang akan di-scrape
+url = 'https://pgkreload.com/history'
+
+# Melakukan scraping dan mengambil data tabel
+table_data = scrape_table(url)
+
+# Menampilkan data tabel menggunakan Streamlit
+st.table(table_data)
