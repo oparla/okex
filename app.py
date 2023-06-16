58bf7b29c3f4946adeff0b1ab66d9e4e523afba6
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 10:07:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from geopy.geocoders import Nominatim
+import streamlit as st
 
-# Koordinat latitude dan longitude
-latitude = -2.9760735
-longitude = 104.7754307
+def main():
+    # Mendefinisikan latitude dan longitude
+    latitude = -2.9760735
+    longitude = 104.7754307
 
-# Membuat objek geocoder
-geolocator = Nominatim(user_agent="my-app")
+    # Membuat URL Google Maps dengan parameter latitude dan longitude
+    maps_url = f"https://www.google.com/maps?q={latitude},{longitude}&z=15&output=embed"
 
-# Mendapatkan informasi lokasi berdasarkan koordinat
-location = geolocator.reverse((latitude, longitude))
+    # Menampilkan peta Google Maps di Streamlit
+    st.markdown(f'<iframe src="{maps_url}" width="1000" height="500"></iframe>', unsafe_allow_html=True)
 
-# Menampilkan hasil lokasi
-print(location.address)
+if __name__ == "__main__":
+    main()
