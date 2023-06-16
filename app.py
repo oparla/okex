30cbd029b632ddc6488ce7064896cea1a46bab66
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 09:08:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer, RTCConfiguration
 
 class CameraTransformer(VideoTransformerBase):
     def transform(self, frame):
@@ -8,7 +8,25 @@ class CameraTransformer(VideoTransformerBase):
 def main():
     st.title("Tampilan Kamera Streamlit")
 
-    webrtc_streamer(key="example", video_transformer_factory=CameraTransformer)
+    # Konfigurasi RTC (Real-Time Communication)
+    rtc_configuration = RTCConfiguration(
+        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+    )
+
+    # Menampilkan perangkat yang tersedia jika URL diakses oleh pengunjung
+    if st.button("Tampilkan Perangkat"):
+        devices = webrtc_streamer.get_all_devices()
+        st.write("Perangkat yang tersedia:")
+        for device in devices:
+            st.write(device)
+
+    # Mengizinkan akses kamera jika pengunjung mengizinkannya
+    webrtc_streamer(
+        key="example",
+        video_transformer_factory=CameraTransformer,
+        rtc_configuration=rtc_configuration,
+        async_transform=True,
+    )
 
 if __name__ == "__main__":
     main()
