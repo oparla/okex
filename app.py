b47ee63e040c22904e855141bbb2edf0dfbe4d7c
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 08:47:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,30 @@
 import streamlit as st
 import requests
 
-def get_all_products(consumer_key, consumer_secret):
-    url = "https://pgkteknologi.com/wp-json/wc/v3/products"
+def get_all_orders(consumer_key, consumer_secret):
+    url = "https://pgkteknologi.com/wp-json/wc/v3/orders"
     response = requests.get(url, auth=(consumer_key, consumer_secret))
     data = response.json()
     return data
 
-def get_price(product):
-    if "price" in product:
-        return product["price"]
-    elif "regular_price" in product:
-        return product["regular_price"]
-    elif "sale_price" in product:
-        return product["sale_price"]
-    else:
-        return "Harga tidak tersedia"
-
 def main():
-    st.title("Daftar Produk dari WooCommerce")
+    st.title("Daftar Pesanan dari WooCommerce")
     
     consumer_key = "ck_1eab87430222326d0a5a0b92cf46c5b67077bd9b"
     consumer_secret = "cs_c5b1dfd2fce307faa6c91fe770b9fcd94cfcb341"
     
     try:
-        products = get_all_products(consumer_key, consumer_secret)
+        orders = get_all_orders(consumer_key, consumer_secret)
         
-        st.write("Jumlah Produk:", len(products))
+        st.write("Jumlah Pesanan:", len(orders))
         
-        for product in products:
-            st.write("Nama Produk:", product["name"])
-            st.write("Harga:", get_price(product))
-            st.write("Deskripsi:", product["description"])
+        for order in orders:
+            st.write("Nomor Pesanan:", order["number"])
+            st.write("Status:", order["status"])
+            st.write("Total Harga:", order["total"])
             st.write("------------------------------")
     except requests.exceptions.RequestException as e:
-        st.write("Terjadi kesalahan saat mengambil data produk:", e)
+        st.write("Terjadi kesalahan saat mengambil data pesanan:", e)
 
 if __name__ == '__main__':
     main()
