7337e84ef2b9d4c3c693cfce222fbbd18895187a
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 09:38:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import streamlit as st
-import time
-
 # Fungsi untuk membuat animasi balon meledak
 def explode_balloon():
-    balloon = st.balloons()
-    time.sleep(2)  # Jeda 2 detik untuk menampilkan balon
     st.balloons(balloon)  # Menampilkan balon meledak
 
 # Memanggil fungsi explode_balloon untuk memulai animasi meledak
