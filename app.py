0332382e6ac888610682cfbfeb2ee6f19939a59d
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 09:43:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
-import time
 
-# Menampilkan animasi balon
-st.balloons()
+# URL video YouTube
+video_url = "https://www.youtube.com/watch?v=lXM1igUSkJM&pp=ygUGdGhvbWFz"
 
-# Memberikan jeda selama 2 detik sebelum menampilkan balon meledak
-time.sleep(2)
+# Menampilkan video dari YouTube dalam mode layar penuh dengan pemutaran otomatis
+st.video(video_url, start_time=0, format='auto', start_paused=True)
 
-# Menghapus animasi balon
-st.balloons(0)
+# Mengubah tata letak aplikasi menjadi layar penuh
+st.markdown('<style>body{margin: 0;}</style>', unsafe_allow_html=True)
