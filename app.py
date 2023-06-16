2d77fb07f7739384f8a82eab08064c46be71429e
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 09:39:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,3 @@ import streamlit as st
 # Fungsi untuk membuat animasi balon meledak
 def explode_balloon():
     st.balloons(balloon)  # Menampilkan balon meledak
-
-# Memanggil fungsi explode_balloon untuk memulai animasi meledak
-explode_balloon()
