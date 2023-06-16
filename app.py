953f112b5b6b048482e5cb7901fa414490d130c8
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 09:35:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import time
 
-st.balloons()
-return()
+# Fungsi untuk menampilkan balon berulang-ulang
+def display_balloons():
+    st.balloons()
+    time.sleep(1)  # Jeda 1 detik
+    display_balloons()  # Memanggil kembali fungsi display_balloons untuk membuat balon berulang-ulang
+
+# Memanggil fungsi display_balloons untuk memulai animasi balon
+display_balloons()
