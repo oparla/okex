dd193a700e1d0f5fc0812168a7e0faf0f2a1850a
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 09:20:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,66 @@
 import streamlit as st
-import requests
+import websocket
+import json
+import pandas as pd
+import plotly.graph_objects as go
 
-def get_indodax_price(pair):
-    url = f'https://indodax.com/api/ticker/{pair}'
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        return data['ticker']['last']
-    else:
-        st.error(f'Error: {response.status_code}')
-        return None
+# Fungsi untuk mengirim pesan ke WebSocket
+def send_message(ws, message):
+    ws.send(json.dumps(message))
 
-# Mendapatkan harga BAT dan BCD
-bat_price = get_indodax_price('batidr')
-bcd_price = get_indodax_price('bcdidr')
+# Fungsi untuk memproses pesan yang diterima dari WebSocket
+def process_message(ws, message):
+    data = json.loads(message)
+    pair = data['market']
+    price = data['last']
 
-# Menampilkan harga menggunakan Streamlit
-if bat_price is not None:
-    st.write('Harga BAT (Basic Attention Token) di Indodax:')
-    st.write(f'IDR {bat_price}')
+    if pair == 'batidr':
+        global bat_prices
+        bat_prices.append(price)
+    elif pair == 'bcdidr':
+        global bcd_prices
+        bcd_prices.append(price)
 
-if bcd_price is not None:
-    st.write('Harga BCD (Bitcoin Diamond) di Indodax:')
-    st.write(f'IDR {bcd_price}')
+# Callback function saat WebSocket terhubung
+def on_open(ws):
+    # Subscribe ke pasangan BAT-IDR dan BCD-IDR
+    send_message(ws, {'action': 'subscribe', 'channel': 'trades', 'pair': 'batidr'})
+    send_message(ws, {'action': 'subscribe', 'channel': 'trades', 'pair': 'bcdidr'})
+
+# Callback function saat pesan diterima dari WebSocket
+def on_message(ws, message):
+    process_message(ws, message)
+
+# Membuat WebSocket connection ke Indodax
+ws = websocket.WebSocketApp('wss://kline.indodax.com/ws/',
+                            on_open=on_open,
+                            on_message=on_message)
+
+# List untuk menyimpan harga
+bat_prices = []
+bcd_prices = []
+
+# Membuat layout Streamlit
+st.title('Harga Cryptocurrency di Indodax')
+st.subheader('BAT (Basic Attention Token) - IDR')
+st.subheader('BCD (Bitcoin Diamond) - IDR')
+
+# Membuka WebSocket connection
+ws.run_forever()
+
+# Menampilkan grafik harga menggunakan Streamlit
+if bat_prices:
+    df_bat = pd.DataFrame({'Price': bat_prices})
+    df_bat['Time'] = pd.to_datetime('now')
+
+    fig_bat = go.Figure(data=go.Scatter(x=df_bat['Time'], y=df_bat['Price']))
+    fig_bat.update_layout(title='Grafik Harga BAT (Basic Attention Token)', xaxis_title='Time', yaxis_title='Price (IDR)')
+    st.plotly_chart(fig_bat, use_container_width=True)
+
+if bcd_prices:
+    df_bcd = pd.DataFrame({'Price': bcd_prices})
+    df_bcd['Time'] = pd.to_datetime('now')
+
+    fig_bcd = go.Figure(data=go.Scatter(x=df_bcd['Time'], y=df_bcd['Price']))
+    fig_bcd.update_layout(title='Grafik Harga BCD (Bitcoin Diamond)', xaxis_title='Time', yaxis_title='Price (IDR)')
+    st.plotly_chart(fig_bcd, use_container_width=True)
