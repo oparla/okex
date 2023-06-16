98d84daab7e58996b921e7ec6d3ec84c4479d4a1
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 09:38:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import time
 
-# Fungsi untuk menampilkan balon berulang-ulang
-def display_balloons():
-    st.balloons()
-    time.sleep(1)  # Jeda 1 detik
-    display_balloons()  # Memanggil kembali fungsi display_balloons untuk membuat balon berulang-ulang
+# Fungsi untuk membuat animasi balon meledak
+def explode_balloon():
+    balloon = st.balloons()
+    time.sleep(2)  # Jeda 2 detik untuk menampilkan balon
+    st.balloons(balloon)  # Menampilkan balon meledak
 
-# Memanggil fungsi display_balloons untuk memulai animasi balon
-display_balloons()
+# Memanggil fungsi explode_balloon untuk memulai animasi meledak
+explode_balloon()
