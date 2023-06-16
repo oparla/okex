68d74e8ce8ef168eda0160745de1be0e310435b1
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 08:49:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,14 @@
 import streamlit as st
-import requests
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
-def get_all_orders(consumer_key, consumer_secret):
-    url = "https://pgkteknologi.com/wp-json/wc/v3/orders"
-    response = requests.get(url, auth=(consumer_key, consumer_secret))
-    data = response.json()
-    return data
+class CameraTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        return frame
 
 def main():
-    st.title("Daftar Pesanan dari WooCommerce")
-    
-    consumer_key = "ck_1eab87430222326d0a5a0b92cf46c5b67077bd9b"
-    consumer_secret = "cs_c5b1dfd2fce307faa6c91fe770b9fcd94cfcb341"
-    
-    try:
-        orders = get_all_orders(consumer_key, consumer_secret)
-        
-        st.write("Jumlah Pesanan:", len(orders))
-        
-        for order in orders:
-            st.write("Nomor Pesanan:", order["number"])
-            st.write("Status:", order["status"])
-            st.write("Total Harga:", order["total"])
-            st.write("------------------------------")
-    except requests.exceptions.RequestException as e:
-        st.write("Terjadi kesalahan saat mengambil data pesanan:", e)
+    st.title("Tampilan Kamera Streamlit")
 
-if __name__ == '__main__':
+    webrtc_streamer(key="example", video_transformer_factory=CameraTransformer)
+
+if __name__ == "__main__":
     main()
