ec5cc22b836a13a9bd648769841de7ad2c308451
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 09:17:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
 import streamlit as st
 import pandas as pd
-
-# Import library untuk melakukan HTTP request
 import requests
 from bs4 import BeautifulSoup
 
-# Fungsi untuk melakukan scraping dan mengambil data tabel
 def scrape_table(url):
-    # Mengirim HTTP GET request ke URL
     response = requests.get(url)
-    
-    # Mengecek status code response
     if response.status_code == 200:
-        # Membuat objek BeautifulSoup untuk melakukan parsing HTML
         soup = BeautifulSoup(response.content, 'html.parser')
-        
-        # Mencari tabel berdasarkan class
-        table = soup.find('table', class_='table table-striped')
-        
-        # Membaca tabel dan mengkonversikannya menjadi DataFrame
-        df = pd.read_html(str(table))[0]
-        
+        table = soup.find('table')
+        rows = table.find_all('tr')
+
+        # Mengambil judul kolom
+        header_row = rows[0]
+        headers = [header.get_text(strip=True) for header in header_row.find_all('th')]
+
+        # Mengambil data baris
+        data_rows = rows[1:]
+        data = []
+        for row in data_rows:
+            cells = row.find_all('td')
+            row_data = [cell.get_text(strip=True) for cell in cells]
+            data.append(row_data)
+
+        # Membuat DataFrame dari data tabel
+        df = pd.DataFrame(data, columns=headers)
         return df
     else:
         st.error(f'Error: {response.status_code}')
+        return None
 
-# URL yang akan di-scrape
 url = 'https://pgkreload.com/history'
-
-# Melakukan scraping dan mengambil data tabel
 table_data = scrape_table(url)
 
-# Menampilkan data tabel menggunakan Streamlit
-st.table(table_data)
+if table_data is not None:
+    st.table(table_data)
