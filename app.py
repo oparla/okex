1e38d29a7d67f61a6ec3a9a4dd3fa57aa7144475
oparla <134549017+oparla@users.noreply.github.com>
2023-06-16 10:08:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ def main():
     latitude = -2.9760735
     longitude = 104.7754307
 
-    # Membuat URL Google Maps dengan parameter latitude dan longitude
-    maps_url = f"https://www.google.com/maps?q={latitude},{longitude}&z=15&output=embed"
+    # Membuat URL Google Maps dengan parameter latitude, longitude, dan zoom level
+    maps_url = f"https://www.google.com/maps?q={latitude},{longitude}&z=18&output=embed"
 
     # Menampilkan peta Google Maps di Streamlit
     st.markdown(f'<iframe src="{maps_url}" width="1000" height="500"></iframe>', unsafe_allow_html=True)
